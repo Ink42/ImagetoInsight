b51acf076ef9231c8518d8547f6831bb2b84d271
lmashele023b <lmashele023b@student.wethinkcode.co.za>
2024-11-02 18:09:23 +0200

    added Falconsai text_summarization model

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from PIL import Image
 import pytesseract
- 
-def load_image():
+from transformers import pipeline
 
-    image_path = 'rrrrrrrrrr.png'
+model ="Falconsai/text_summarization"
+task ="summarization"
+
+def load_image(image_path:str):
     image = Image.open(image_path)
+    text:str = pytesseract.image_to_string(image)
+    return summarizer(text)
 
-    text = pytesseract.image_to_string(image)
+def summarizer(text:str)-> str:
+    # Is i encounted here was that it would not return an interger rather it would return a float causing errors hence the reason i ended up using round to turn the output into a whole number
 
-
-    print(text)
-    
+    max_length:int = round(len(text.split(" ")) / 2)
+    min_length:int = round(max_length/2)
+    summarizer = pipeline(task, model=model)
+    return summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)[0]['summary_text']
 
 if __name__=="__main__":
-    pass
+   print(load_image("sample_images/ww.jpeg"))
